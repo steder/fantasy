87f27d7552e5128e4f43c8e11736c9ff3b03f744
Mike Steder <steder@gmail.com>
2014-10-11 10:56:28 -0500

    Start making JSON requests from the bookmarklet; enable CORS

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask.json import jsonify
+from flask.ext.cors import CORS
 from sqlalchemy import create_engine
 from sqlalchemy import text
 
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+cors = CORS(app)
 
 
 engine = create_engine('postgresql://127.0.0.1/fantasy', echo=True)
--- static/bookmarklet.js
+++ static/bookmarklet.js
@@ -2,7 +2,22 @@ var Fantasy = (function (me, $) {
   me.helloWorld = function () {
     alert("Hello World!");
   };
+
+  me.loadStats = function() {
+    var week = "6";
+    var promise = $.getJSON("https://localhost:5000/stats/" + week).
+        done(function(data) {
+          console.log("Successfully got player data:");
+          console.log(data.players[0]);
+        }).
+        fail(function(err) {
+          console.log("Failed to get player data!");
+        });
+    return promise;
+  }
+
   return me;
 }(Fantasy || {}, jQuery));
 
 Fantasy.helloWorld();
+Fantasy.loadStats();
